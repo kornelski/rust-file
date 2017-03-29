ae3794068369f2e631d6b87b421a1d3434350403
Kornel Lesiński <kornel@geekhood.net>
2017-03-29 10:18:49 +0100

    Docs

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # File I/O 1-liners
 
-Read and write `Vec<u8>` with one function call.
+## `Vec<u8>`
+
+`file::get()` and `file::put()` — read and write `Vec<u8>` with one function call.
 
 ```rust
 extern crate file;
 
-fn main() {
-    let data = file::get("some_input_file.dat").unwrap();
-    file::put("a.out", &data).unwrap();
+fn example() -> file::Result<()> {
+    let data = file::get("some_input_file.dat")?;
+    file::put("a.out", &data)?;
+    Ok(())
 }
 ```
+
+`file::Result` is an alias for `std::io::Result`. You can use `Result<(), Box<std::error::Error>>` in places where you don't want to expose the error type.
+
+## `String`
+
+`file::get_text()` and `file::put_text()` — read and write `String` with one function call.
+
+```rust
+extern crate file;
+
+fn example() -> file::Result<()> {
+    let string = file::get_text("hello.txt")?;
+    file::put("bye.txt", &string)?;
+    Ok(())
+}
+```
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,39 @@
+//! # File I/O 1-liners
+//!
+//! `file::get()` and `file::put()` — read and write `Vec<u8>` with one function call.
+//!
+//! ```rust
+//! extern crate file;
+//!
+//! fn example() -> file::Result<()> {
+//!     let data = file::get("some_input_file.dat")?;
+//!     file::put("a.out", &data)?;
+//!     Ok(())
+//! }
+//! ```
+//!
+//! `file::Result` is an alias for `std::io::Result`. You can use `Result<(), Box<std::error::Error>>` in places where you don't want to expose the error type.
+//!
+//! ## Text file 1-liners
+//!
+//! `file::get_text()` and `file::put_text()` — read and write `String` with one function call.
+//!
+//! ```rust
+//! extern crate file;
+//!
+//! fn example() -> file::Result<()> {
+//!     let string = file::get_text("hello.txt")?;
+//!     file::put("bye.txt", &string)?;
+//!     Ok(())
+//! }
+//! ```
+
 use std::fs::File;
 use std::path::Path;
 use std::io;
 use std::io::{Read, Write};
+
+pub use io::Result;
 
 /// Read a file into `Vec<u8>` from the given path.
 /// The path can be a string or a `Path`.
